14553c4c189bd7cf2600761e0b69130c70c1c2fd
Bruno Melo <bruno.melo@idor.org>
2017-02-22 13:42:58 -0300

    Fixing init script

--- init.m
+++ init.m
@@ -4,8 +4,7 @@ function init()
     path_only = p(1:seps(end));
 
     addpath(path_only);
-    addpathsubdirs(path_only, {'core', 'MASKS', 'model_definitions', 'regressor_utils', 'utils'});
-    addpath( genpath( fullfile( path_only, 'batch_modules' ) ) );
+    addpathsubdirs(path_only, {'batch_modules', 'core', 'model_definitions', 'regressor_utils', 'utils'});
 end
 
 function addpathsubdirs( pathD, subdirs)
