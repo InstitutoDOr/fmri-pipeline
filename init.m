f9f2582b5f003392407185d8c055018cab2a75e1
Bruno Melo <bruno.melo@idor.org>
2017-02-22 15:38:36 -0300

    Fixing dependency usage.

--- init.m
+++ init.m
@@ -1,14 +1,8 @@
-function init()
-    p = mfilename('fullpath');
-    seps = strfind( p, filesep );
-    path_only = p(1:seps(end));
-
-    addpath(path_only);
-    addpathsubdirs(path_only, {'batch_modules', 'core', 'model_definitions', 'regressor_utils', 'utils'});
-end
-
-function addpathsubdirs( pathD, subdirs)
-    for k=1:length(subdirs)
-        addpath(fullfile(pathD,subdirs{k}));
-    end
-end
+run('../matlab-utils/init.m');
+includeSubdirs( {
+    'batch_modules'
+    'core'
+    'model_definitions'
+    'regressor_utils'
+    'utils'
+});
